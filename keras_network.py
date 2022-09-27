c189e0d8bbdeca48428bb8a4af74f1b6cbd2c487
manorajesh <mano@raje.sh>
2022-09-27 11:56:10 -0700

    Added average function

--- keras_network.py
+++ keras_network.py
@@ -2,8 +2,15 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 import numpy as np
 
-inputs = np.array([[2, 3], [3, 10], [6, 8], [100, 82]])
-target = np.array([[2.5, 6.5, 7, 91]])
+inputs = np.array([])
+targets = np.array([])
+
+# Create data set to predict average of 2 numbers
+for i in range(100):
+    rand_pair = np.random.randint(10, size=2)
+    inputs = np.append(inputs, rand_pair)
+    inputs = np.reshape(inputs, (-1, 2))
+    targets = np.append(targets, np.average(inputs[i]))
 
 model = Sequential()
 model.add(Dense(2, input_shape=(2,)))
@@ -16,4 +23,4 @@ model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
 model.summary()
 
 test = np.array([[2, 3]])
-model.predict(test)
+print(model.predict(test))
