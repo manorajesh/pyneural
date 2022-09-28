2be48d8a71170bef9b3ea7b3b4b791c08f0fd417
manorajesh <mano@raje.sh>
2022-09-27 21:54:48 -0700

    iterating

--- isprime.py
+++ isprime.py
@@ -17,16 +17,12 @@ for i in range(100000):
     inputs = np.append(inputs, rand_int)
     targets = np.append(targets, is_prime(rand_int))
 
-model = Sequential()
-model.add(Input(shape=(1,)))
-model.add(Dense(2, input_shape=(1,), activation='relu'))
-model.add(Dense(3, activation='relu'))
-model.add(Dense(4, activation='relu'))
-model.add(Dense(1, activation='relu'))
+model = Sequential([Input(shape=(1,)), Dense(100), Activation('sigmoid')])
 
 model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
+model.summary()
 model.fit(inputs, targets, validation_split=0.1, epochs=2, use_multiprocessing=True)
 
-test = np.array([4])
+test = np.array([5])
 print(model.predict(test))
 print(model.evaluate(test))
