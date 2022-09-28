0a240b0631a429ea8472fb284599812c318f724b
manorajesh <mano@raje.sh>
2022-09-27 21:44:33 -0700

    added isprime.py to the mix

---
+++ isprime.py
@@ -0,0 +1,28 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras import Input
+import numpy as np
+from math import sqrt
+
+def is_prime(n):
+  for i in range(2,int(sqrt(n))+1): # O(sqrt(n))
+    if (n%i) == 0:
+      return False
+  return True
+
+inputs = np.array([])
+targets = np.array([])
+for i in range(100000):
+    rand_int = np.random.randint(1000000)
+    inputs = np.append(inputs, rand_int)
+    targets = np.append(targets, is_prime(rand_int))
+
+model = Sequential()
+model.add(Input(shape=(1,)))
+model.add(Dense(2, input_shape=(1,), activation='relu'))
+model.add(Dense(3, activation='relu'))
+model.add(Dense(4, activation='relu'))
+model.add(Dense(1, activation='relu'))
+
+model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
+model.fit(inputs, targets, validation_split=0.1, epochs=2, use_multiprocessing=True)
--- keras_network.py
+++ keras_network.py
@@ -6,21 +6,27 @@ inputs = np.array([])
 targets = np.array([])
 
 # Create data set to predict average of 2 numbers
-for i in range(100):
-    rand_pair = np.random.randint(10, size=2)
+for i in range(100000):
+    rand_pair = np.random.randint(100, size=2)
     inputs = np.append(inputs, rand_pair)
     inputs = np.reshape(inputs, (-1, 2))
     targets = np.append(targets, np.average(inputs[i]))
 
 model = Sequential()
 model.add(Dense(2, input_shape=(2,)))
-model.add(Activation('sigmoid'))
+for i in range(10):
+    model.add(Dense(2))
+    model.add(Activation('sigmoid'))
+
+#model.add(Activation('sigmoid'))
 model.add(Dense(1))
-model.add(Activation('sigmoid'))
+#model.add(Activation('sigmoid'))
 
-model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy']) 
+model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
+model.fit(inputs, targets, validation_split=0.1, epochs=2, use_multiprocessing=True)
 
-model.summary()
+#model.summary()
 
-test = np.array([[2, 3]])
+test = np.array([[4, 8]])
 print(model.predict(test))
+print(model.evaluate(test))
